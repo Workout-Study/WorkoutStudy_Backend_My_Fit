4b8772aeb85b749016646124206788f53023877d
정정일 <105261146+12OneTwo12@users.noreply.github.com>
2024-04-09 14:04:27 +0900

    Update fit group end point

--- src/main/kotlin/com/fitmate/myfit/adapter/out/api/uris/FitGroupUri.kt
+++ src/main/kotlin/com/fitmate/myfit/adapter/out/api/uris/FitGroupUri.kt
@@ -3,7 +3,7 @@ package com.fitmate.myfit.adapter.out.api.uris
 class FitGroupUri {
 
     companion object {
-        const val FIT_GROUP_ROOT_URI = "http://localhost:8000/fit-group-service"
+        const val FIT_GROUP_ROOT_URI = "http://fit-group:8080/fit-group-service"
 
         const val GROUP_ROOT = "$FIT_GROUP_ROOT_URI/groups"
         const val MATE_ROOT = "$FIT_GROUP_ROOT_URI/mates"
