0b801876f5f557c5c9d04eb66578262696c3edb8
Jeong Jeongil <jji042842@gmail.com>
2024-06-03 10:26:42 +0900

    user event to info event

--- src/main/kotlin/com/fitmate/myfit/MyFitApplication.kt
+++ src/main/kotlin/com/fitmate/myfit/MyFitApplication.kt
@@ -3,8 +3,10 @@ package com.fitmate.myfit
 import com.fitmate.myfit.common.GlobalStatus
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class MyFitApplication
 
 fun main(args: Array<String>) {
