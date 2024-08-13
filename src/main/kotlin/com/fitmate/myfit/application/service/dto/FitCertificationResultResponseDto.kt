5709cab0a55afff5c751aac75e7abeed475229e4
Jeong Jeongil <jji042842@gmail.com>
2024-08-13 18:26:40 +0900

    instant to string

--- src/main/kotlin/com/fitmate/myfit/application/service/dto/FitCertificationResultResponseDto.kt
+++ src/main/kotlin/com/fitmate/myfit/application/service/dto/FitCertificationResultResponseDto.kt
@@ -1,10 +1,13 @@
 package com.fitmate.myfit.application.service.dto
 
+import com.fitmate.myfit.adapter.out.api.DateParseUtils
 import com.fitmate.myfit.domain.CertificationStatus
 import java.time.Instant
 
 data class FitCertificationResultResponseDto(
     val fitCertificationId: Long,
     val certificationStatus: CertificationStatus,
-    val createdAt: Instant
-)
+    val createdAt: String
+) {
+    val createdAtInstant: Instant = DateParseUtils.stringToInstant(createdAt)
+}
