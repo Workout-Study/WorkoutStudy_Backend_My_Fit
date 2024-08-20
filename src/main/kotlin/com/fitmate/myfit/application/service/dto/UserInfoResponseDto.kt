270028a50e74728b97d90d3bd8e686f95b46cd6a
12OneTwo12 <jji042842@gmail.com>
2024-08-20 23:35:32 +0900

    change user service url

--- src/main/kotlin/com/fitmate/myfit/application/service/dto/UserInfoResponseDto.kt
+++ src/main/kotlin/com/fitmate/myfit/application/service/dto/UserInfoResponseDto.kt
@@ -1,12 +1,18 @@
 package com.fitmate.myfit.application.service.dto
 
+import com.fitmate.myfit.adapter.out.api.DateParseUtils
+import org.springframework.util.StringUtils
 import java.time.Instant
 
-data class UserInfoResponseDto(
+data class UserInfoResponse(
     val userId: Int,
     val nickname: String,
     val state: Boolean,
-    val imageUrl: String,
-    val createdAt: Instant,
-    val updatedAt: Instant
-)
+    val imageUrl: String?,
+    val createdAt: String,
+    val updatedAt: String?
+) {
+    val createdAtInstant = DateParseUtils.stringToInstant(createdAt)
+    val updatedAtInstant: Instant? =
+        if (StringUtils.hasText(updatedAt)) DateParseUtils.stringToInstant(updatedAt!!) else null
+}
